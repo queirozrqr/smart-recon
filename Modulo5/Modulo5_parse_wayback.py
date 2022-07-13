790292d220cf57211d5c2c40594fe1d489782130
BraXploiter <81781383+MasterAdolf@users.noreply.github.com>
2022-07-13 19:27:28 -0300

    Resolvendo Problemas de Decode da saída wayback
    Algum caracter não identificado está sujando o primeiro índice da lista que é passada para a variável sistema, assim, anulamos o erro que esse caracter NÃO NULO gera.

--- Modulo5/Modulo5_parse_wayback.py
+++ Modulo5/Modulo5_parse_wayback.py
@@ -34,43 +34,46 @@ def executa(url2):
 def parse():
 	list_sistemas = executa(url2)
 	for sistema in list_sistemas:
-		if(sistema != '' or sistema != None):
-			dic_web['network.protocol'] = sistema.split(':')[0]
-			try:
-				dic_web['server.port'] = sistema.split(':')[2].split('/')[0]
-			except:
-				if(dic_web['network.protocol'] == 'http'):
-					dic_web['server.port'] = '80'
+		try:
+			if(sistema != '' or sistema != None):
+				dic_web['network.protocol'] = sistema.split(':')[0]
+				try:
+					dic_web['server.port'] = sistema.split(':')[2].split('/')[0]
+				except:
+					if(dic_web['network.protocol'] == 'http'):
+						dic_web['server.port'] = '80'
+					else:
+						dic_web['server.port'] = '443'
+				path = len(sistema.split('/'))
+				if(path == 3):
+					dic_web['url.path'] = '/'
+					dic_web['url.original'] = sistema
 				else:
-					dic_web['server.port'] = '443'
-			path = len(sistema.split('/'))
-			if(path == 3):
-				dic_web['url.path'] = '/'
-				dic_web['url.original'] = sistema
-			else:
-				i = 3
-				dic_web['url.path'] = ''
-				dic_web['url.original'] = dic_web['network.protocol']+'://'+sistema.split('/')[2]
-				while i < path:
-					dic_web['url.path'] = dic_web['url.path']+'/'+sistema.split('/')[i]
-					i += 1
+					i = 3
+					dic_web['url.path'] = ''
+					dic_web['url.original'] = dic_web['network.protocol']+'://'+sistema.split('/')[2]
+					while i < path:
+						dic_web['url.path'] = dic_web['url.path']+'/'+sistema.split('/')[i]
+						i += 1
 
-			data = {
-			'@timestamp': hora,
-			'server.address': subdomain,
-			'server.domain': subdomain,
-			'server.ip': ip,
-			'server.port': dic_web['server.port'],
-			'network.protocol': dic_web['network.protocol'],
-			'url.path': dic_web['url.path'],
-			'http.response.status_code': '200',
-			'url.original': dic_web['url.original'],
-			'url.full': dic_web['url.original']+dic_web['url.path'],
-			'vulnerability.scanner.vendor': scanner
-			}	
-			#print(data)
-			r = requests.post(url, headers=headers, auth=auth, data=json.dumps(data), verify=False)
-			print (r.text)
+				data = {
+				'@timestamp': hora,
+				'server.address': subdomain,
+				'server.domain': subdomain,
+				'server.ip': ip,
+				'server.port': dic_web['server.port'],
+				'network.protocol': dic_web['network.protocol'],
+				'url.path': dic_web['url.path'],
+				'http.response.status_code': '200',
+				'url.original': dic_web['url.original'],
+				'url.full': dic_web['url.original']+dic_web['url.path'],
+				'vulnerability.scanner.vendor': scanner
+				}	
+				#print(data)
+				r = requests.post(url, headers=headers, auth=auth, data=json.dumps(data), verify=False)
+				print (r.text)
+		except:
+			pass
 def main():
     parse()
     
